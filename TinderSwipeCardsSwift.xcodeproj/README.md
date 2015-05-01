5afce742d925da16002bc1878ea3fc9d65371d8f
reterVision <reterclose@gmail.com>
2015-05-01 10:22:54 +0800

    Implement Tinder swipe cards effect in Swift.

---
+++ TinderSwipeCardsSwift.xcodeproj/README.md
@@ -0,0 +1,6 @@
+TinderSwipeCardsSwift
+======
+
+### Credits to: https://github.com/cwRichardKim/TinderSimpleSwipeCards
+
+Implement Tinder swipe cards effect in Swift.
---
+++ TinderSwipeCardsSwift/ViewController.swift
@@ -0,0 +1,28 @@
+//
+//  ViewController.swift
+//  TinderSwipeCardsSwift
+//
+//  Created by Gao Chao on 4/30/15.
+//  Copyright (c) 2015 gcweb. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+
+        var draggableBackground: DraggableViewBackground = DraggableViewBackground(frame: self.view.frame)
+        self.view.addSubview(draggableBackground)
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
